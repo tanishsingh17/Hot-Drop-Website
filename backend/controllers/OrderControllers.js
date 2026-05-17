746988d072450610bffbc78cc5aaef64dbd83fe9
Tanish Singh Thakur <144647913+tanishsingh17@users.noreply.github.com>
2026-05-17 14:49:48 +0530

    Update OrderControllers.js

--- backend/controllers/OrderControllers.js
+++ backend/controllers/OrderControllers.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order for frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "https://hot-drop-website-frontend.onrender.com"
 
     try {
         const newOrder = new OrderModels({
